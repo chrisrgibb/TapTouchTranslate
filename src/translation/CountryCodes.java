bc539be0d587dae0efc20df492aa97775938639a
Chris Gibb <chrisrgibb@gmail.com>
2014-03-21 17:15:20 +1300

    added Country Codes, passing tests

---
+++ src/translation/CountryCodes.java
@@ -0,0 +1,17 @@
+package translation;
+
+import java.util.HashMap;
+
+public class CountryCodes {
+	
+	
+	public static HashMap<String, String> codes = new HashMap<String, String>();
+	
+	public CountryCodes(){
+		codes.put("English", "eng");
+		
+		
+	}
+	
+	
+}
